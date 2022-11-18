58e03193281cf177a76b580604966ce6fdc4f798
Leo Huppert <leo@huppert.io>
2022-11-18 23:57:37 +0100

    feat: update to HUE api v2

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
 # Hue server
 
-First get your Hue bridge IP address : https://discovery.meethue.com/
+## Outdated way of getting key and ip
 
-Then get a user token to interact with the bridge API, execute this POST request with your name and bridge IP :
+> First get your Hue bridge IP address : https://discovery.meethue.com/
+
+> Then get a user token to interact with the bridge API, execute this POST request with your name and bridge IP :
 
 ```
 curl -X POST -d {\"devicetype\":\"my_hue_app#<NAME>\"} <BRIDGE_IP>/api
 ```
 
-Press the link button on the bridge and execute it again. Keep the token you were sent back.
+> Press the link button on the bridge and execute it again. Keep the token you were sent back.
+
+## env file
 
 Create a `.env` file at the project root and set these variables
 
 ```
-HUE_USERNAME=<YOUR_USERNAME>
-HUE_IP=http:/<BRIDGE_IP>/api
+HUE_BRIDGE_API=<the bridge ip>
+HUE_APPLICATION_KEY=<your application key>
 ```
 
 You can start the API !
--- index.js
+++ index.js
@@ -2,49 +2,58 @@ const { default: axios } = require("axios");
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const https = require("https");
 require("dotenv").config();
 
 const app = express();
 const PORT = 9000;
-const USERNAME = process.env.HUE_USERNAME;
-const HUE_IP = process.env.HUE_IP;
+const BASE_URL = `https://${process.env.HUE_BRIDGE_API}/clip/v2/resource`;
+
+const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+const axiosInstance = axios.create({ baseURL: BASE_URL, httpsAgent });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    config.headers["hue-application-key"] = process.env.HUE_APPLICATION_KEY;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 
 app.get("/lights", (req, res) => {
-  axios
-    .get(`${HUE_IP}/${USERNAME}/lights`)
-    .then(({ data }) => res.send(Object.values(data)));
+  axiosInstance.get(`${BASE_URL}/light`).then(({ data }) => res.send(data));
 });
 
-app.get("/lights/:id", (req, res) => {
-  axios
-    .get(`${HUE_IP}/${USERNAME}/lights/${req.params.id}`)
+app.get("/rooms", (req, res) => {
+  axiosInstance.get(`${BASE_URL}/room`).then(({ data }) => {
+    res.send(data);
+  });
+});
+
+app.get("/scenes", (req, res) => {
+  axiosInstance.get(`${BASE_URL}/scene`).then(({ data }) => res.send(data));
+});
+
+app.get("/light/:id", (req, res) => {
+  axiosInstance
+    .get(`${BASE_URL}/light/${req.params.id}`)
     .then(({ data }) => res.send(data));
 });
 
-app.put("/lights/:id/toggle", (req, res) => {
-  axios
-    .put(`${HUE_IP}/${USERNAME}/lights/${req.params.id}/state`, {
-      on: req.body.on,
-    })
+app.put("/light/:id/toggle", (req, res) => {
+  axiosInstance
+    .put(`${BASE_URL}/light/${req.params.id}`, { on: { on: req.body.on } })
     .then(({ data }) => res.send(data));
 });
 
-app.put("/lights/:id/brightness", (req, res) => {
-  axios
-    .put(`${HUE_IP}/${USERNAME}/lights/${req.params.id}/state`, {
-      bri: +req.body.brightness,
-    })
-    .then(({ data }) => res.send(data));
-});
-
-app.put("/lights/:id/hue", (req, res) => {
-  axios
-    .put(`${HUE_IP}/${USERNAME}/lights/${req.params.id}/state`, {
-      hue: +req.body.hue,
+app.put("/light/:id/brightness", (req, res) => {
+  axiosInstance
+    .put(`${BASE_URL}/light/${req.params.id}`, {
+      dimming: { brightness: req.body.brightness },
     })
     .then(({ data }) => res.send(data));
 });
